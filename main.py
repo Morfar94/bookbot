6fa090847e3f94e1d4faf6d5030735ca18cdb9cd
Morfar94 <Ravn94@live.dk>
2025-06-02 21:07:10 +0200

    letter counter

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from stats import get_book_text
-
 from stats import count_words
+from stats import count_letters
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_amount = count_words(book_text)
+    letter_amount = count_letters(book_text)
     print(f'{word_amount} words found in the document')
+    print(letter_amount)
 
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,13 @@ def get_book_text(path_to_file):
 def count_words(book_text):
     word_amount = len(book_text.split())
     return word_amount
+
+def count_letters(book_text):
+    lower_text = book_text.lower()
+    letters_dictionary = {}
+    for letter in lower_text:
+        if letter in letters_dictionary:
+            letters_dictionary[letter] = letters_dictionary[letter] + 1
+        else:
+            letters_dictionary[letter] = 1
+    return letters_dictionary
