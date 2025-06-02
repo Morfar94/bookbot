fa894049b044e61f997474ae95d39c0b9fc14316
Morfar94 <Ravn94@live.dk>
2025-06-02 20:44:37 +0200

    fstring implementeret

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ def count_words(book_text):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_amount = count_words(book_text)
-    print(word_amount + ' words found in the document')
-    
+    print(f'{word_amount} words found in the document')
+
 main()
