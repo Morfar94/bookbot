4bd086f3f70af90282153eb9fc70fcd449a803fa
Morfar94 <Ravn94@live.dk>
2025-06-02 21:52:32 +0200

    list of letters printed

--- main.py
+++ main.py
@@ -9,7 +9,18 @@ def main():
     word_amount = count_words(book_text)
     letter_amount = count_letters(book_text)
     list_of_chars = reformat_dict(letter_amount)
-    print(f'{word_amount} words found in the document')
-    print(list_of_chars)
+    print('============ BOOKBOT ============')
+    print('Analyzing book found at books/frankenstein.txt...')
+    print('----------- Word Count ----------')
+    print(f'Found {word_amount} total words')
+    print('--------- Character Count -------')
+
+    list_of_chars.sort(reverse=True,key=sort_on)
+    
+    for item in list_of_chars:
+        amount = item['num']
+        character = item['char']
+        if character.isalpha():
+            print(f'{character}: {amount}')
 
 main()
