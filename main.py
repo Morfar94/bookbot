effaaf228b368c6ef598bc52cd69a691d585f7bd
Morfar94 <Ravn94@live.dk>
2025-06-02 20:34:53 +0200

    v3

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    print(get_book_text("/books/frankenstein.txt"))
-
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+    
 main()
